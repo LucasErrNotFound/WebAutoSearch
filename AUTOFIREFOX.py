c971aab299f3a0035414017344b56ddde6dd5d9f
LucasErrNotFound <132194439+LucasErrNotFound@users.noreply.github.com>
2025-09-14 21:58:02 +0800

    refactor: fix indentation by 4 spaces, remove blank spaces

--- AUTOFIREFOX.py
+++ AUTOFIREFOX.py
@@ -8,13 +8,11 @@ from selenium.webdriver.common.keys import Keys
 import WebAutoSearch
 
 def process3():
+    driver = webdriver.Firefox()                # Launches GeckoDriver
+    driver.set_window_size(1106, 1080)          # Setting the windows size
+    driver.get("https://www.duckduckgo.com")    # Setting duckduckgo as its search engine
 
-    driver = webdriver.Firefox()                #Launches GeckoDriver
-    driver.set_window_size(1106, 1080)          #Setting the windows size
-    driver.get("https://www.duckduckgo.com")    #Setting duckduckgo as its search engine
-
-    def fire():     #All available hotkeys
-
+    def fire():     # All available hotkeys
         AUTO_SEARCH_HOTKEY = [
             {keyboard.Key.alt_l}
         ]
@@ -26,11 +24,9 @@ def process3():
         current = set()
 
         def on_press(key):
-
             if any([key in HOT for HOT in AUTO_SEARCH_HOTKEY]):
                 current.add(key)
                 if any(all(k in current for k in HOT) for HOT in AUTO_SEARCH_HOTKEY):
-
                     data = pyperclip.paste()
                     pyperclip.copy(data)
 
@@ -41,7 +37,6 @@ def process3():
                         fire()
 
                     except selenium.common.exceptions.NoSuchElementException:
-
                         TOP_SEARCH_BOX = driver.find_element(By.ID, "search_form_input")
                         TOP_SEARCH_BOX.send_keys(Keys.SHIFT + Keys.HOME + Keys.BACKSPACE)
                         TOP_SEARCH_BOX.send_keys(data)
@@ -51,16 +46,15 @@ def process3():
             elif any([key in HOT for HOT in QUIT_KEY]):
                 current.add(key)
                 if any(all(k in current for k in HOT) for HOT in QUIT_KEY):
-                    driver.quit()           #This kills the GeckoDriver process
-                    L.stop()                #This stops the keyboard listener (Again, This is NOT a keylogger, so calm the fuck down) - 6H0$T
-                    print("Exiting...")     #When user presses 'esc',
-                    WebAutoSearch.main()    #It goes back to the main menu
-                    os._exit(0)             #And kills this process
+                    driver.quit()           # This kills the GeckoDriver process
+                    L.stop()                # This stops the keyboard listener (Again, This is NOT a keylogger, so calm the fuck down) - 6H0$T
+                    print("Exiting...")     # When user presses 'esc',
+                    WebAutoSearch.main()    # It goes back to the main menu
+                    os._exit(0)             # And kills this process
 
         def on_release(key):
             if any([key in HOT for HOT in AUTO_SEARCH_HOTKEY]):
                 current.remove(key)
-
             elif any([key in HOT for HOT in QUIT_KEY]):
                 current.remove(key)
 
