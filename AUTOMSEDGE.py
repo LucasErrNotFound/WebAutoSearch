ab8194a39441f722eef58e6ec8d4c2c662f52910
LucasErrNotFound <132194439+LucasErrNotFound@users.noreply.github.com>
2025-09-14 21:59:04 +0800

    refactor: fix indentation by 4 spaces, remove blank spaces

--- AUTOMSEDGE.py
+++ AUTOMSEDGE.py
@@ -8,13 +8,11 @@ from selenium.webdriver.common.keys import Keys
 import WebAutoSearch
 
 def process2():
+    driver = webdriver.Edge()               # Launches MsEdgeDriver
+    driver.set_window_size(1106, 1080)      # Setting the window size
+    driver.get("https://www.bing.com")      # Setting bing as its search engine
 
-    driver = webdriver.Edge()               #Launches MsEdgeDriver
-    driver.set_window_size(1106, 1080)      #Setting the window size
-    driver.get("https://www.bing.com")      #Setting bing as its search engine
-
-    def edge():     #All available hotkeys
-
+    def edge():     # All available hotkeys
         AUTO_SEARCH_HOTKEY = [
             {keyboard.Key.alt_l}
         ]
@@ -26,16 +24,13 @@ def process2():
         current = set()
 
         def on_press(key):
-
             if any([key in HOT for HOT in AUTO_SEARCH_HOTKEY]):
                 current.add(key)
                 if any(all(k in current for k in HOT) for HOT in AUTO_SEARCH_HOTKEY):
-
                     data = pyperclip.paste()
                     pyperclip.copy(data)
 
                     try:
-
                         TOP_SEARCH_BOX = driver.find_element(By.ID, "sb_form_q")
                         TOP_SEARCH_BOX.send_keys(Keys.SHIFT + Keys.HOME + Keys.BACKSPACE)
                         TOP_SEARCH_BOX.send_keys(data)
@@ -43,29 +38,24 @@ def process2():
                         edge()
 
                     except selenium.common.exceptions.NoSuchElementException:
-
                         print("Error: I can't seem to find the XPATH")
-
-
 
             elif any([key in HOT for HOT in QUIT_KEY]):
                 current.add(key)
                 if any(all(k in current for k in HOT) for HOT in QUIT_KEY):
-
-                    driver.quit()           #This kills the MsEdgeDriver process
-                    L.stop()                #This stops the keyboard listener (This is not a keylogger, so calm the fuck down) - 6H0$T
-                    print("Exiting...")     #When user presses 'esc',
-                    WebAutoSearch.main()    #It goes back to the main menu
-                    os._exit(0)             #And kills this process
+                    driver.quit()           # This kills the MsEdgeDriver process
+                    L.stop()                # This stops the keyboard listener (This is not a keylogger, so calm the fuck down) - 6H0$T
+                    print("Exiting...")     # When user presses 'esc',
+                    WebAutoSearch.main()    # It goes back to the main menu
+                    os._exit(0)             # And kills this process
 
         def on_release(key):
             if any([key in HOT for HOT in AUTO_SEARCH_HOTKEY]):
                 current.remove(key)
-
             elif any([key in HOT for HOT in QUIT_KEY]):
                 current.remove(key)
 
-        with keyboard.Listener(on_press = on_press, on_release = on_release) as L:
+        with keyboard.Listener(on_press=on_press, on_release=on_release) as L:
             L.join()
 
     edge()
